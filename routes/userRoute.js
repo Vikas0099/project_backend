008685636b955fbae25d95ce254d01ef78041a2a
Vikas Sharma <vikas03172@gmail.com>
2025-05-04 23:30:52 +0530

    final commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ import {registerUser,loginUser, getProfile,updateProfile,bookAppointment,listApp
 import authUser from "../middlewares/authUser.js";
 import upload from "../middlewares/multer.js";
 
+
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
@@ -12,6 +13,7 @@ userRouter.post("/update-profile", upload.single('image'),authUser,updateProfile
 userRouter.post("/book-appointment",authUser,bookAppointment);
 userRouter.get("/appointments",authUser, listAppointment);
 userRouter.post("/cancel-appointment",authUser,cancelAppointment);
+
 
 
 
